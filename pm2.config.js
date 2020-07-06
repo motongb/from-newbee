6d36d8ce63f767c6ab78b8215381892ddb74fd34
motb <18589998412@163.com>
2020-07-06 21:53:00 +0800

    fix pm2

--- pm2.config.js
+++ pm2.config.js
@@ -6,8 +6,8 @@ module.exports = {
             args: [
                 '-jar',
                 'target/newbee-mall.jar',
-                '--server.port=80',
-                '--upload.path=E:\\upload\\'
+                '--server.port=8000',
+                '--upload.path=/root/upload/'
             ],
             cwd: '.',
             interpreter: ''
