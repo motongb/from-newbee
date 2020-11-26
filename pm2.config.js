f1dc0112d45d7fd099e4611f1eca764c35a83dd3
motongb <38830159+motongb@users.noreply.github.com>
2020-11-26 09:36:06 +0800

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -7,6 +7,9 @@ module.exports = {
                 '-jar',
                 'target/newbee-mall.jar',
                 '--server.port=8000',
+                '--spring.datasource.url=jdbc:mysql://localhost:3306/newbee_mall_db?useUnicode=true&serverTimezone=Asia/Shanghai&characterEncoding=utf8&autoReconnect=true&useSSL=false&allowMultiQueries=true',
+                '--spring.datasource.username=root',
+                '--spring.datasource.password=123456',
                 '--upload.path=/root/upload/'
             ],
             cwd: '.',
